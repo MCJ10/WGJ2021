27756f8b0c7f495af381aa1f979d4c7ad2fd58aa
Karen Da Cruz <me@karendacruz.com>
2021-08-21 18:24:18 -0500

    Player constrained to camera boundary

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -22,7 +22,8 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 targetPos = SetPos(cameraTargetPos, target.position.x, target.position.y + cameraOffsetY, -1);
+        // Vector3 targetPos = SetPos(cameraTargetPos, target.position.x, target.position.y + cameraOffsetY, -1);
+        Vector3 targetPos = SetPos(cameraTargetPos, Camera.main.transform.position.x, target.position.y + cameraOffsetY, -1);
         transform.position = Vector3.Lerp(transform.position, targetPos, 0.2f);
 
         if (transform.position.y >= bg2.position.y)
