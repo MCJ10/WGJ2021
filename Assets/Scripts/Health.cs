35f9a107e8847694b19adea1f419dc5aed13b9cb
Karen Da Cruz <me@karendacruz.com>
2021-08-22 01:53:34 -0500

    Particle animation when player is killed

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,9 +7,13 @@ public class Health : MonoBehaviour
     public int maxHealth;
     [SerializeField]
     private int currentHealth;
+
+    private UnityEngine.Object explosionRef;
+
     void Start()
     {
         currentHealth = maxHealth;
+        explosionRef = Resources.Load("Explosion");
     }
 
     // Update is called once per frame
@@ -20,6 +24,8 @@ public class Health : MonoBehaviour
         currentHealth -= damage;
         if (currentHealth <= 0)
         {
+            GameObject explosion = (GameObject)Instantiate(explosionRef);
+            explosion.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             gameObject.SetActive(false);
         }
 
